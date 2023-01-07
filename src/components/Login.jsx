c4f4206ab11519a63728b87627387e0b35304b4d
Pedro Roberto Bezerra De Medeiros <pedropwks@gmail.com>
2023-01-07 16:56:11 -0300

    Adiciona requisito 2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import * as userAPI from '../services/userAPI';
 
 class Login extends Component {
+  state = {
+    inputName: '',
+    isLoading: false,
+  };
+
+  handleName = ({ target }) => {
+    this.setState({
+      inputName: target.value,
+    });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const { inputName } = this.state;
+    const { history } = this.props;
+
+    this.setState({
+      isLoading: true,
+    });
+
+    await userAPI.createUser({ name: inputName });
+    history.push('/search');
+  };
+
   render() {
+    const { inputName, isLoading } = this.state;
+    const numberThree = 3;
+
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <h1>LOGIN</h1>
+        <input data-testid="login-name-input" type="text" onChange={ this.handleName } />
+        <button
+          data-testid="login-submit-button"
+          type="submit"
+          disabled={ inputName.length < numberThree }
+          onClick={ this.handleSubmit }
+        >
+          Entrar
+
+        </button>
+        { isLoading ? <h1>Carregando...</h1> : '' }
+      </div>
     );
   }
 }
 
 export default Login;
+
+Login.propTypes = {
+  history: PropTypes.object,
+}.isRequired;
