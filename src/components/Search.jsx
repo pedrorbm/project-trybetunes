a4ccdaa94f580909de6743d6c4811f4b41dda9de
Pedro Roberto Bezerra De Medeiros <pedropwks@gmail.com>
2023-01-09 19:04:34 -0300

    Adiciona requisito 5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class Search extends Component {
+  state = {
+    search: '',
+  };
+
+  searchInput = ({ target }) => {
+    this.setState({
+      search: target.value,
+    });
+  };
+
   render() {
+    const { search } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            onChange={ this.searchInput }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ search.length < 2 }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
