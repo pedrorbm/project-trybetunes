322a5ab7d2c4bb3a1f7d6c5e4d3fc2f79b72bfdd
Pedro Roberto Bezerra De Medeiros <pedropwks@gmail.com>
2023-01-14 18:16:51 -0300

    Adiciona requisito 8

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import * as favoriteSongsAPI from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
+  state = {
+    isLoading: false,
+  };
+
+  handleClick = async () => {
+    const { arrayMusic } = this.props;
+
+    this.setState({
+      isLoading: true,
+    });
+
+    try {
+      await favoriteSongsAPI.addSong(arrayMusic);
+      this.setState({
+        isLoading: false,
+      });
+    } catch {
+      this.setState({
+        isLoading: false,
+      });
+    }
+  };
+
   render() {
     const { arrayMusic } = this.props;
+    const { isLoading } = this.state;
 
     return (
       <div>
@@ -16,6 +42,19 @@ class MusicCard extends Component {
           <code>audio</code>
           .
         </audio>
+        <label
+          data-testid={ `checkbox-music-${arrayMusic.trackId}` }
+          htmlFor={ arrayMusic.trackId }
+        >
+          <input
+            type="checkbox"
+            name="favorite"
+            id={ arrayMusic.trackId }
+            onClick={ this.handleClick }
+          />
+          Favorita
+        </label>
+        { isLoading ? <Loading /> : undefined }
       </div>
     );
   }
